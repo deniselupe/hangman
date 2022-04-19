11273a161b253debda87e3360bf87fed6085c7dd
deniselupe <dgrodriguez24@hotmail.com>
2022-04-19 20:10:18 +0000

    feat: prompt player for guess of the next letter

--- game.rb
+++ game.rb
@@ -9,4 +9,22 @@ class Game
     @turns_left = 8
     @winner = false
   end
+
+  # The game loops until there are no turns left or until the player wins
+  def play_game
+    until @turns_left.negative? || @winner == true
+      guess = input_guess
+    end
+  end
+
+  # This prompts the player to make their next guess
+  def input_guess
+    print "\nWhat's the next letter? "
+
+    until (guess = gets.chomp.downcase).match?(/^[a-z]{1}$/)
+      print "\nYou can only guess one letter at a time. Please try again: "
+    end
+
+    guess
+  end
 end
